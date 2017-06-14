55b9dcb9f2e6b307bd2cd826e39cb53fe6dc4bf6
shawnrushefsky <shawn@psymphonic.com>
2017-06-14 18:18:18 -0500

    Outlining browser tests
    also minor bug fixes and typos

--- tests/browsertests.js
+++ tests/browsertests.js
@@ -1,10 +1,37 @@
-var webdriver = require('selenium-webdriver'),
-    By = webdriver.By,
-    until = webdriver.until;
+var webdriver = require('selenium-webdriver')
+var chai = require('chai')
 
-var driver = new webdriver.Builder()
-    .forBrowser('chrome')
-    .build();
+var expect = chai.expect
 
-driver.get("https://localhost")
-console.log(driver)
+describe("Browser Interactions", function(){
+  describe("Logging in", function(){
+    it("types in username and password and clicks enter", function(){
+
+    })
+  })
+  describe("Creating a new user", function(){
+    it("creates an admin user", function(done){
+      done()
+    })
+    it("creates a non-admin user", function(done){
+      done()
+    })
+  })
+  describe("Adding a new expense", function(){
+    
+  })
+  describe("Retrieving expenses", function(){
+    describe("Retrieving all expenses", function(){
+
+    })
+    describe("Retrieving expenses within a range of datetimes", function(){
+
+    })
+  })
+  describe("Editing an existing epxense", function(){
+
+  })
+  describe("Deleting an expense", function(){
+
+  })
+})
